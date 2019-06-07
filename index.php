5414c38c287396fb9d0560cb12ffbdd7b1e3c908
Your Name <you@example.com>
2019-06-07 13:59:55 +0700

    Bài tập xây dựng lớp StopWatch

--- index.php
+++ index.php
@@ -46,7 +46,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>[Bài tập] Xây dựng lớp StopWatch</title>
+    <title>Bài tập xây dựng lớp StopWatch</title>
     <style>
         form{
             margin-left: 35%;
